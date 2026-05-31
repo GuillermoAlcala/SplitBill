ebd455d69f888110d222b3b8159f837987c76526
GuillermoAlcala <alcalamemo@gmail.com>
2026-05-31 09:29:36 -0600

    new elements

---
+++ SplitBill/Resources/CardPerson.swift
@@ -0,0 +1,18 @@
+//
+//  CardPerson.swift
+//  SplitBill
+//
+//  Created by GuillermoChaconAlcala on 30/05/26.
+//
+
+import SwiftUI
+
+struct CardPerson: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardPerson()
+}
---
+++ SplitBill/Resources/sampleSwipe.swift
@@ -0,0 +1,8 @@
+//
+//  sampleSwipe.swift
+//  SplitBill
+//
+//  Created by GuillermoChaconAlcala on 30/05/26.
+//
+
+import Foundation
