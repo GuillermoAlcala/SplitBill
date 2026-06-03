9ee2e8e20fb49e5257e81c1cd454772bca9a7757
GuillermoAlcala <alcalamemo@gmail.com>
2026-06-03 13:44:04 -0600

    new changes

---
+++ SplitBill/Views/ContentView2.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView2.swift
+//  SplitBill
+//
+//  Created by GuillermoChaconAlcala on 01/06/26.
+//
+
+import SwiftUI
+
+struct ContentView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView2()
+}
---
+++ SplitBill/Views/CreateSplitView2.swift
@@ -0,0 +1,18 @@
+//
+//  CreateSplitView2.swift
+//  SplitBill
+//
+//  Created by GuillermoChaconAlcala on 31/05/26.
+//
+
+import SwiftUI
+
+struct CreateSplitView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateSplitView2()
+}
